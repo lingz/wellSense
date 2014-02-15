0de5398d6219f74b899b88ac004c5af269934553
Koh Terai <rootrootrootrootrootroot@gmail.com>
2014-02-16 03:09:13 +0400

    comment section logos and formatting complete

--- client/views/comments/comments.js
+++ client/views/comments/comments.js
@@ -4,5 +4,25 @@ Template.comments.helpers({
   },
   comments: function() {
     return Comments.find({shortcode: this.shortcode}, {sort: {timestamp: -1}});
+  },
+  imgSrc: function() {
+  	switch (this.flag){
+	  	case "general":
+	  		return "/icon-general.png"
+	  	case "event":
+	  		return "/icon-event.png"
+	  	case "breakdown":
+	  		return "/icon-breakdown.png"
+	  	case "maintenance":
+	  		return "/icon-maintenance.png"
+	  	case "repair":
+	  		return "/icon-repair.png"
+  	}
+  },
+  timeHeader: function(){
+    var newTime = moment(this.time).format('MMMM Do, YYYY');
+    var flag = this.flag;
+    newTime += (' - ' + flag + ' Comment')
+    return newTime
   }
 });
